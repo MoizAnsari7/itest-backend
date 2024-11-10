0d7f68680a7e25fce9b7eaa3e945e49a933effe0
MoizAnsari7 <moizansari176@gmail.com>
2024-11-10 09:53:46 -0500

    Middleware Added

--- models/Question.model.js
+++ models/Question.model.js
@@ -5,9 +5,9 @@ const questionSchema = new mongoose.Schema({
     questionText: { type: String, required: true },
     questionType: { type: mongoose.Schema.Types.ObjectId, ref: 'QuestionType', required: true },
     options: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Option' }],
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    difficulty: { type: String, enum: ['easy', 'medium', 'hard'], default: 'medium' }, // New field
-    isLibraryQuestion: { type: Boolean, default: false } // New field
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to the creator
+    difficulty: { type: String, enum: ['easy', 'medium', 'hard'], default: 'medium' },
+    isLibraryQuestion: { type: Boolean, default: true }
 });
 
 module.exports = mongoose.model('Question', questionSchema);
